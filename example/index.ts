7ecf894ada39c2080cdf767968d49eb54dd65129
vignesh anand <vigneshanand17@gmail.com>
2019-03-16 23:04:28 +0530

    Added example page

---
+++ example/index.ts
@@ -0,0 +1,3 @@
+import scorer from "../index";
+const result = scorer.score(document.body.innerHTML, document);
+console.log(result);
---
+++ test.ts
@@ -0,0 +1,7 @@
+import test from "ava";
+import scorer from "./index";
+
+test("foo", t => {
+  console.log("scorer");
+  t.pass();
+});
