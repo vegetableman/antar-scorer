0ebfc542aa59eae4e71d5d393d3ea52821ac6acb
vignesh anand <vigneshanand17@gmail.com>
2019-04-05 01:01:14 +0530

    Removed readability example code

--- example/index.ts
+++ example/index.ts
@@ -1,8 +1,7 @@
-import Readability from "./readability";
 import scorer from "../index";
 const result = scorer.score(document.body.innerHTML, document);
-console.log(result);
 
-var documentClone = document.cloneNode(true);
-var article = new Readability(<HTMLDocument>documentClone, {}).parse();
-console.log(article);
+// Compare with readability
+// import Readability from "./readability";
+// var documentClone = document.cloneNode(true);
+// var article = new Readability(<HTMLDocument>documentClone, {}).parse();
